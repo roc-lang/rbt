53f1eb19d999fe3a4d76566dbdfef4396be40d4e
Richard Feldman <oss@rtfeldman.com>
2021-06-15 09:35:23 -0400

    Improve cache and make it testable

--- src/cache.rs
+++ src/cache.rs
@@ -22,12 +22,14 @@ struct MetaKey {
 impl MetaKey {
     pub fn persist(&self, _path: &Path) -> io::Result<()> {
         // TODO store a (Path => MetaKey) dictionary entry on disk.
-        todo!();
+        // For now, don't persist at all.
+        Ok(())
     }
 
-    pub fn stored(_path: &Path) -> io::Result<Self> {
+    pub fn stored(_path: &Path) -> io::Result<Option<Self>> {
         // TODO get the stored MetaKey from the (Path => MetaKey) dictionary on disk
-        todo!();
+        // For now, assume nothing was stored.
+        Ok(None)
     }
 
     pub fn current(path: &Path) -> io::Result<Self> {
@@ -55,7 +57,7 @@ impl MetaKey {
 
 #[derive(Default)]
 pub struct Cache {
-    hashes: HashMap<FileId, ContentHash>,
+    by_file_id: HashMap<FileId, (MetaKey, ContentHash)>,
 }
 
 impl Cache {
@@ -80,10 +82,14 @@ impl Cache {
     }
 
     /// Check whether the content of the file ID changed.
-    /// This is done by reading it from disk and computing its content hash.
-    /// If it changed, return its current ContentHash (that is, the hash that
-    /// differed from the one stored in the cache).
-    /// If it hasn't changed, return None.
+    /// This is done by reading it from disk and seeing if the file's
+    /// metadata changed since the last time we looked at it. If the metadata
+    /// is the same, we assume the contents have not changed, and we
+    /// return a cached ContentHash. If the metadata is different, we
+    /// read the contents of the file from disk and hash them into a ContentHash,
+    /// then cache that ContentHash under the metadata for future use.
+    ///
+    /// If the ContentHash hasn't changed, return None.
     ///
     /// This operation is not atomic! If the file changes
     /// (according to `notify`) in the middle of this operation, this will
@@ -98,65 +104,71 @@ impl Cache {
 
         // If the file's current metadata is the same as the last one we
         // recorded on disk, then we can reasonably conclude it hasn't changed.
-        let current_meta_key = {
-            let prev_meta_key = MetaKey::stored(path)?;
-            let current_meta_key = MetaKey::current(path)?;
+        let prev_meta_key = MetaKey::stored(path)?;
+        let current_meta_key = MetaKey::current(path)?;
 
-            if current_meta_key == prev_meta_key {
-                return Ok(None);
-            }
-
-            current_meta_key
-        };
-
-        // The metadata was different, so the file may have changed.
-        // Proceed with computing the ContentHash from the file's contents!
-
-        // Read the file from the file system and hash it
-        let current_hash = ContentHash::from_file(path)?;
-
-        // To find the previous ContentHash for this FileId, try the in-memory
-        // ContentHash cache first before going to the on-disk cache.
-        let prev_hash = match self.hashes.get(&file_id) {
-            Some(prev_hash) => *prev_hash,
-            None => {
-                // We don't have this one in memory, so
-                // try the on-disk cache.
-                match Self::lookup_on_disk(path)? {
-                    Some(prev_hash) => {
-                        // Save the on-disk hash in our in-memory cache, so
-                        // we don't have to read it from disk again next time.
-                        self.hashes.insert(file_id, prev_hash);
-
-                        prev_hash
-                    }
-                    None => {
-                        // We've never hashed this file before.
-                        //
-                        // Store it in both the in-memory cache
-                        // as well as on disk for future runs.
-                        self.hashes.insert(file_id, current_hash);
-                        Self::persist(path, current_hash)?;
-
-                        // Since we've never seen it before, consider it changed.
-                        return Ok(Some(current_hash));
-                    }
-                }
-            }
-        };
-
-        // Now that we've made it past the point where we might have returned
-        // early with an io::Err, we should record the new MetaKey. This way,
-        // the next time we ask whether this path has changed, we'll be
-        // considering it relative to the ContentHash we're about to return.
-        current_meta_key.persist(path)?;
-
-        if current_hash == prev_hash {
-            // The file's content has not changed.
+        if Some(current_meta_key) == prev_meta_key {
             Ok(None)
         } else {
-            // The file's content hash has changed!
-            Ok(Some(current_hash))
+            // The metadata was different, so the file may have changed.
+            // Proceed with computing the ContentHash from the file's contents!
+
+            // Read the file from the file system and hash it
+            let current_hash = ContentHash::from_file(path)?;
+            let prev_hash;
+
+            // To find the previous ContentHash for this FileId, try the in-memory
+            // ContentHash cache first before going to the on-disk cache.
+            match self.by_file_id.get(&file_id) {
+                Some((_stale_meta_key, hash)) => {
+                    prev_hash = Some(*hash);
+
+                    // Record the new MetaKey in the in-memory cache.
+                    self.by_file_id
+                        .insert(file_id, (current_meta_key, current_hash));
+                }
+                None => {
+                    // We don't have this one in memory, so
+                    // try the on-disk cache.
+                    match Self::lookup_on_disk(path)? {
+                        Some(hash) => {
+                            // Save the on-disk hash in our in-memory cache, so
+                            // we don't have to read it from disk again next time.
+                            self.by_file_id.insert(file_id, (current_meta_key, hash));
+
+                            prev_hash = Some(hash);
+                        }
+                        None => {
+                            // We've never hashed this file before.
+                            //
+                            // Store it in both the in-memory cache
+                            // as well as on disk for future runs.
+                            self.by_file_id
+                                .insert(file_id, (current_meta_key, current_hash));
+                            Self::persist(path, current_hash)?;
+
+                            // We've never seen this content before. This will
+                            // have the effect that we end up considering it
+                            // changed!
+                            prev_hash = None;
+                        }
+                    }
+                }
+            };
+
+            // Now that we've made it past the point where we might have returned
+            // early with an io::Err, we should record the new MetaKey. This way,
+            // the next time we ask whether this path has changed, we'll be
+            // considering it relative to the ContentHash we're about to return.
+            current_meta_key.persist(path)?;
+
+            if Some(current_hash) == prev_hash {
+                // The file's content has not changed.
+                Ok(None)
+            } else {
+                // The file's content hash has changed!
+                Ok(Some(current_hash))
+            }
         }
     }
 
@@ -166,7 +178,7 @@ impl Cache {
         // then compare the file metadata to that file's current metadata; if
         // it's unchanged, then we can use the given ContentHash.
         // If that has an entry, then we have our
-        todo!();
+        Ok(None)
     }
 
     fn persist(_path: &Path, _hash: ContentHash) -> io::Result<()> {
@@ -177,6 +189,6 @@ impl Cache {
         //
         // TODO: how can we make renames efficient without invalidating the old
         // hashes? e.g. so if we switch branches, we don't have to rebuild everything?
-        todo!();
+        Ok(())
     }
 }
