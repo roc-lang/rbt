a89a7b80a7184cfb4bf30f81a93b70e4d3833b10
Brian Hicks <brian@brianthicks.com>
2021-06-15 09:25:08 -0500

    stop looping, you

--- src/job.rs
+++ src/job.rs
@@ -31,18 +31,25 @@ impl Job {
 mod test_job {
     use super::Job;
     use std::collections::HashMap;
+    use tempfile::tempdir;
 
     #[test]
     fn produces_output() {
+        let tmp = tempdir().unwrap();
+        let dest = tmp.path().join("hello.txt");
+
         let job = Job {
             command: "bash".to_string(),
-            arguments: vec!["-c".to_string(), "echo Hello, World > file.txt".to_string()],
+            arguments: vec![
+                "-c".to_string(),
+                format!("echo Hello, World > {}", dest.to_str().unwrap()),
+            ],
             environment: HashMap::default(),
         };
 
         assert_eq!(job.run().unwrap(), ());
 
-        let contents = std::fs::read_to_string("file.txt").unwrap();
+        let contents = std::fs::read_to_string(dest).unwrap();
         assert_eq!(contents.as_str(), "Hello, World\n");
     }
 
