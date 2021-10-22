6f965d6f1017ecfdc58c0c7d9b48553dac0d9fbf
Brian Hicks <brian@brianthicks.com>
2021-10-22 14:37:14 -0500

    get rid of RocCallResult

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod job;
 
 use core::ffi::c_void;
 use core::mem::MaybeUninit;
-use roc_std::{RocCallResult, RocList, RocStr};
+use roc_std::{RocList, RocStr};
 use std::collections::HashMap;
 use std::path::PathBuf;
 use std::process::Command;
@@ -20,7 +20,7 @@ struct RocJob {
 
 extern "C" {
     #[link_name = "roc__mainForHost_1_exposed"]
-    fn roc_main(output: *mut RocCallResult<RocJob>) -> ();
+    fn roc_main(output: *mut RocJob) -> ();
 }
 
 #[no_mangle]
@@ -45,53 +45,48 @@ pub unsafe fn roc_dealloc(c_ptr: *mut c_void, _alignment: u32) {
 
 #[no_mangle]
 pub fn rust_main() -> isize {
-    let mut call_result: MaybeUninit<RocCallResult<RocJob>> = MaybeUninit::uninit();
+    let mut job: MaybeUninit<RocJob> = MaybeUninit::uninit();
 
     unsafe {
-        roc_main(call_result.as_mut_ptr());
+        roc_main(job.as_mut_ptr());
 
-        let output = call_result.assume_init();
+        let roc_job = job.assume_init();
 
-        match output.into() {
-            Ok(roc_job) => {
-                let args: Vec<String> = roc_job
-                    .arguments
-                    .as_slice()
-                    .iter()
-                    .map(|file| file.as_str().to_string())
-                    .collect();
+        let args: Vec<String> = roc_job
+            .arguments
+            .as_slice()
+            .iter()
+            .map(|file| file.as_str().to_string())
+            .collect();
 
-                let inputs: Vec<PathBuf> = roc_job
-                    .inputs
-                    .as_slice()
-                    .iter()
-                    .map(|path| PathBuf::from(path.as_str()))
-                    .collect();
+        let inputs: Vec<PathBuf> = roc_job
+            .inputs
+            .as_slice()
+            .iter()
+            .map(|path| PathBuf::from(path.as_str()))
+            .collect();
 
-                let outputs: Vec<PathBuf> = roc_job
-                    .outputs
-                    .as_slice()
-                    .iter()
-                    .map(|path| PathBuf::from(path.as_str()))
-                    .collect();
+        let outputs: Vec<PathBuf> = roc_job
+            .outputs
+            .as_slice()
+            .iter()
+            .map(|path| PathBuf::from(path.as_str()))
+            .collect();
 
-                let job = job::Job {
-                    // TODO: these should eventually be RocStrs, and Job should
-                    // just accept and convert those accordingly.
-                    command: roc_job.command.as_str().to_string(),
-                    arguments: args.clone(),
-                    environment: HashMap::default(),
-                    working_directory: PathBuf::from(roc_job.working_directory.as_str()),
-                    inputs: inputs,
-                    outputs: outputs,
-                };
-                job.run().expect("TODO better platform error handling");
-            }
-            Err(msg) => {
-                panic!("Roc failed with message: {}", msg);
-            }
-        }
+        let job = job::Job {
+            // TODO: these should eventually be RocStrs, and Job should
+            // just accept and convert those accordingly.
+            command: roc_job.command.as_str().to_string(),
+            arguments: args.clone(),
+            environment: HashMap::default(),
+            working_directory: PathBuf::from(roc_job.working_directory.as_str()),
+            inputs: inputs,
+            outputs: outputs,
+        };
+        job.run().expect("TODO better platform error handling");
     }
+
+    println!("All done!");
 
     // Exit code
     0
