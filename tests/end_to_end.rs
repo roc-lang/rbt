15d9c2fb777626b4aeaaf738eabd825cbec6344f
Brian Hicks <brian@brianthicks.com>
2022-09-27 16:08:50 -0500

    use --print-root-output-paths for more reliable end-to-end tests

--- tests/end_to_end.rs
+++ tests/end_to_end.rs
@@ -1,37 +1,47 @@
-use assert_cmd::cmd::Command;
-use std::time::Duration;
+use anyhow::{Context, Result};
+use std::path::{Path, PathBuf};
 use tempfile::TempDir;
+
+fn output_of_default_job(root: &TempDir, rbt_dot_roc: &Path) -> Result<PathBuf> {
+    let current_dir = rbt_dot_roc
+        .parent()
+        .context("failed to get parent of the target rbt.roc")?;
+    let filename = rbt_dot_roc
+        .file_name()
+        .context("failed to get the filename of the target rbt.roc")?;
+
+    let output = std::process::Command::new("roc")
+        .arg("run")
+        .arg("--linker=legacy")
+        .arg(filename)
+        .arg("--")
+        .arg("--root-dir")
+        .arg(root.path().display().to_string())
+        .arg("--print-root-output-paths")
+        .current_dir(current_dir.display().to_string())
+        .output()
+        .context("failed to spawn `roc run`")?;
+
+    if !output.status.success() {
+        anyhow::bail!("failed to `roc run`: {:#?}", output);
+    }
+
+    Ok(PathBuf::from(
+        std::str::from_utf8(&output.stdout)
+            .context("could not convert output to a UTF-8 string")?
+            .trim(),
+    ))
+}
 
 #[test]
 fn test_file_inputs() {
     let root = TempDir::new().unwrap();
 
-    Command::new("roc")
-        .arg("run")
-        .arg("--linker=legacy")
-        .arg("rbt.roc")
-        .arg("--")
-        .arg("--root-dir")
-        .arg(root.path().display().to_string())
-        .current_dir("tests/end_to_end/file_inputs")
-        .timeout(Duration::from_secs(10))
-        .assert()
-        .success();
-
-    let store_path = root
-        .path()
-        .join("store")
-        .read_dir()
-        .expect("could not read dir")
-        .flatten()
-        .filter(|item| {
-            item.file_type()
-                .map(|ftype| ftype.is_dir())
-                .unwrap_or(false)
-        })
-        .map(|item| item.path())
-        .next()
-        .expect("expected at least one directory in the store path");
+    let store_path = output_of_default_job(
+        &root,
+        &PathBuf::from("tests/end_to_end/file_inputs/rbt.roc"),
+    )
+    .unwrap();
 
     let greeting = std::fs::read_to_string(store_path.join("out")).unwrap();
 
@@ -42,32 +52,8 @@ fn test_file_inputs() {
 fn test_env() {
     let root = TempDir::new().unwrap();
 
-    Command::new("roc")
-        .arg("run")
-        .arg("--linker=legacy")
-        .arg("rbt.roc")
-        .arg("--")
-        .arg("--root-dir")
-        .arg(root.path().display().to_string())
-        .current_dir("tests/end_to_end/env")
-        .timeout(Duration::from_secs(10))
-        .assert()
-        .success();
-
-    let store_path = root
-        .path()
-        .join("store")
-        .read_dir()
-        .expect("could not read dir")
-        .flatten()
-        .filter(|item| {
-            item.file_type()
-                .map(|ftype| ftype.is_dir())
-                .unwrap_or(false)
-        })
-        .map(|item| item.path())
-        .next()
-        .expect("expected at least one directory in the store path");
+    let store_path =
+        output_of_default_job(&root, &PathBuf::from("tests/end_to_end/env/rbt.roc")).unwrap();
 
     let greeting = std::fs::read_to_string(store_path.join("out")).unwrap();
 
@@ -78,32 +64,9 @@ fn test_env() {
 fn test_job_inputs() {
     let root = TempDir::new().unwrap();
 
-    Command::new("roc")
-        .arg("run")
-        .arg("--linker=legacy")
-        .arg("rbt.roc")
-        .arg("--")
-        .arg("--root-dir")
-        .arg(root.path().display().to_string())
-        .current_dir("tests/end_to_end/job_inputs")
-        .timeout(Duration::from_secs(10))
-        .assert()
-        .success();
-
-    let store_path = root
-        .path()
-        .join("store")
-        .read_dir()
-        .expect("could not read dir")
-        .flatten()
-        .filter(|item| {
-            item.file_type()
-                .map(|ftype| ftype.is_dir())
-                .unwrap_or(false)
-        })
-        .map(|item| item.path())
-        .next()
-        .expect("expected at least one directory in the store path");
+    let store_path =
+        output_of_default_job(&root, &PathBuf::from("tests/end_to_end/job_inputs/rbt.roc"))
+            .unwrap();
 
     let greeting = std::fs::read_to_string(store_path.join("out")).unwrap();
 
