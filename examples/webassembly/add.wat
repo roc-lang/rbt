a0ffaba20d3d40f6a0d7de5085e39760933d375a
Brian Hicks <brian@brianthicks.com>
2022-10-04 16:27:32 -0500

    remove non-compiling webassembly example

--- examples/webassembly/add.wat
+++
@@ -1,7 +0,0 @@
-(module
-  (func (export "add") (param $n1 i32) (param $n2 i32) (result i32)
-    get_local $n1
-    get_local $n2
-    i32.add
-  )
-)
--- examples/webassembly/index.js
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-import addWasm from "./add.wasm";
-
-WebAssembly.instantiate(addWasm).then(addModule => {
-  console.log("adding 1 and 2 in WebAssembly:")
-  console.log(addModule.instance.exports.add(1, 2));
-})
